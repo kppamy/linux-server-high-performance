d60b9e3f7726cf3fe2545c1a265a26cf00266c4c
chenay <anyan.chen@technicolor.com>
2020-05-19 09:58:34 +0800

    max profit for k chances, cooldown

--- ArrayTest.cpp
+++ ArrayTest.cpp
@@ -16,9 +16,6 @@ int ArrayTest::maxProfit(vector<int> &prices)
     return max;
 }
 
-
-
-
 void ArrayTest::rotateArray(vector<int> &input, int k)
 {
     // extra k steps
@@ -69,9 +66,6 @@ void ArrayTest::rotateOne(vector<int> &input)
     tmp = input[0];
 }
 
-
-
-
 int subMaxProfit(vector<int> &prices, int end, int &index_b, int &index_s)
 {
     if (end == 1)
@@ -89,17 +83,21 @@ int subMaxProfit(vector<int> &prices, int end, int &index_b, int &index_s)
             return 0;
         }
     }
-    if(index_s == -1  ){
-        if(prices[end] > prices[index_b])
+    if (index_s == -1)
+    {
+        if (prices[end] > prices[index_b])
             index_s = end;
-        else if(prices[end] < prices[index_b])
+        else if (prices[end] < prices[index_b])
             index_b = end;
         return 0;
-    }else if (prices[end] > prices[index_s])
+    }
+    else if (prices[end] > prices[index_s])
     {
         index_s = end;
         return (subMaxProfit(prices, end - 1, index_b, index_s) + (prices[end] - prices[index_s]));
-    }else {
+    }
+    else
+    {
         return subMaxProfit(prices, end - 1, index_b, index_s);
     }
 }
@@ -107,39 +105,151 @@ int subMaxProfit(vector<int> &prices, int end, int &index_b, int &index_s)
 // buy and sell only once
 void maxProfitBF(vector<int> &prices)
 {
-    int max=INT_MIN;
-    for(int i=0;i<prices.size()-1;++i){
-        for(int j=i+1;j<prices.size();++j){
-            if(prices[j]-prices[i]>max){
-                max = prices[j]-prices[i];
+    int max = INT_MIN;
+    for (int i = 0; i < prices.size() - 1; ++i)
+    {
+        for (int j = i + 1; j < prices.size(); ++j)
+        {
+            if (prices[j] - prices[i] > max)
+            {
+                max = prices[j] - prices[i];
             }
         }
     }
 
-    cout<<" max profit "<< max<<endl;
+    cout << " max profit " << max << endl;
 }
 
 // buy and sell only once
 void maxProfit1P(vector<int> &prices)
 {
-    int min=INT_MAX;
-    int prf=0;
-    for(int i=0;i<prices.size();++i){
-        if(prices[i]< min){
+    int min = INT_MAX;
+    int prf = 0;
+    for (int i = 0; i < prices.size(); ++i)
+    {
+        if (prices[i] < min)
+        {
             min = prices[i];
-        }else if(prices[i] - min > prf ){
+        }
+        else if (prices[i] - min > prf)
+        {
             prf = prices[i] - min;
         }
     }
-    cout<<" max profit "<<prf<<endl;
+    cout << " max profit " << prf << endl;
 }
 
+// buy and sell for k chances
+void maxProfitDP(vector<int> &prices, int k)
+{
+    int nums = prices.size();
+    vector<vector<int>> profits(k + 1, vector<int>(nums, 0));
+    for (int kk = 1; kk < k + 1; kk++)
+    {
+        int g = 0;
+        int i = 1;
+        int j = 0;
+        for (i = 1; i < nums; ++i)
+        {
+            for (j = 0; j < i; ++j)
+            {
+                g = max(g, profits[kk - 1][j] + prices[i] - prices[j]);
+            }
+            profits[kk][i] = max(profits[kk][i - 1], g);
+        }
+    }
+    // max profit for 2 chances is: 16 elaps: 121518 ms
+    cout << "max profit for " << k << " chances"
+         << " is: " << profits[k][nums - 1];
+
+    //input size: 10W, 2*10 20w loops
+    //  ++i,i++ difference ~1s
+    // create i,j inside the loop or not: diff ~0.5s
+
+    // ++i,++j, create i,j inside the loop
+    // "max profit for 2 chances is: 16elaps: 120787 ms\r\n"
+
+    // i++, j++ dont create i,j inside the loop
+    // @"max profit for 2 chances is: 16elaps: 121053 ms\r\n"
+
+    // i++,j++, create i,j inside the loop
+    // max profit for 2 chances is: 16 elaps: 121518 ms
+}
+
+// buy and sell for k chances
+void maxProfit2chances(vector<int> &prices)
+{
+    int i = 0;
+    int b1 = INT_MAX, b2 = INT_MAX;
+    int s1 = INT_MIN, s2 = INT_MIN;
+    for (; i < prices.size(); i++)
+    {
+        b1 = min(b1, prices[i]);
+        s1 = max(s1, prices[i] - b1);
+        b2 = min(b2, prices[i] - s1);
+        s2 = max(s2, prices[i] - b2);
+    }
+    // max profit for 2 chances is: 16 elaps: 121518 ms
+    cout << "max profit for 2 chances"
+         << " is: " << s2;
+}
+
+void maxProfitCoolDown(const vector<int> &prices)
+{
+    int len = prices.size();
+    if (len <= 1)
+        return;
+    vector<int> prf(len, 0);
+    prf[1] = max(prices[1] - prices[0], 0);
+    if (len == 2)
+    {
+        cout << "max profit for unlimited trasactions but cooldown 1d "
+             << " is: " << prf[len - 1] << endl;
+        return;
+    }
+    prf[2] = max(prf[1], prf[1] + prices[2] - prices[1]);
+    int i = 3, j = 2;
+    int g = 0;
+    for (; i < len; ++i)
+    {
+        for (j = 2; j < i; ++j)
+        {
+            g = max(g, prf[j - 2] + prices[i] - prices[j]);
+            g = max(g, prf[j - 1] + prices[i] - prices[j]);
+        }
+        prf[i] = max(prf[i - 1], g);
+    }
+
+    cout << "max profit for unlimited trasactions but cooldown 1d "
+         << " is: " << prf[len - 1] << endl;
+}
+
+void maxProfitCoolDownSM(const vector<int> &prices)
+{
+    int len = prices.size();
+    vector<int> bought(len, 0);
+    vector<int> sold(len, 0);
+    vector<int> cooldown(len, 0);
+    bought[0] = -prices[0];
+    sold[0] = 0;
+    cooldown[0] = 0;
+    int i;
+    for (i = 1; i < len; ++i)
+    {
+        bought[i] = max(cooldown[i - 1] - prices[i], bought[i - 1]);
+        sold[i] = max(bought[i - 1] + prices[i], sold[i - 1]);
+        cooldown[i] = max(max(sold[i - 1], bought[i - 1]), cooldown[i - 1]);
+    }
+
+    cout << "max profit for unlimited trasactions but cooldown 1d "
+         << " is: " << max(sold[len - 1], cooldown[len - 1]) << endl;
+}
 
 int main(int argc, char const *argv[])
 {
     /* code */
     ArrayTest at;
-    // vector<int> prices{7, 1, 5, 3, 6, 4};
+    // vector<int> prices{4 ,3, 8, 9 ,3 ,7, 7 ,4 ,9 ,1};
     vector<int> prices = generateArray(10, 10);
     // int max = at.maxProfit(prices);
     // cout<<" max profit is: "<<max<<endl;
@@ -149,8 +259,14 @@ int main(int argc, char const *argv[])
     // cout << "after rotate " << k << " k" << endl;
     // at.rotateArraySwitch(prices, k);
     printVector(prices);
+    int start = clock();
 
-    maxProfitBF(prices);
+    // maxProfitDP(prices, 2);
+    // maxProfit2chances(prices);
+    maxProfitCoolDownSM(prices);
 
+    int end = clock();
+    double elaps = (static_cast<float>(end - start)) / CLOCKS_PER_SEC;
+    cout << "elaps: " << elaps * 1000 << " ms" << endl;
     return 0;
 }
