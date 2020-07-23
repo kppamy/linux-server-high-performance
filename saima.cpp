39a97218ba04ce11ac5c6de97540290f2c48405c
chenay <anyan.chen@technicolor.com>
2020-07-23 14:50:47 +0800

    struct memory align

--- saima.cpp
+++ saima.cpp
@@ -12,23 +12,6 @@
 #include <vector>
 using namespace std;
 
-struct Student
-{
-
-    int num;
-
-    char name[7];
-
-    short age;
-
-    char sex;
-
-} student1;
-
-struct Student2
-{
-    char name[7];
-} student2;
 
 #include <iostream>
 #include <algorithm>
@@ -93,67 +76,138 @@ bool match(string input)
     return true;
 }
 
-int main(int argc, char const *argv[])
+void testAlignMemoryOfStruct()
 {
+    struct Student
+    {
+        int num; // 偏移 0
+        char name[7];//偏移 4
+        char sex; // 
+        long age;// 偏移 16
+    } student1; //size 24
 
-    string input;
-    cin>>input;
-    string res= match(input)?"true":"false";
-    cout<<res;
-    return 0;
+     struct Student5
+    {
+        //1. 每个成员按照 max（GCC 4，数据成员长度）的倍数进行对齐
+        // 2. 结构体按照min（GCC 4，最大数据成员长度）的倍数进行对齐 
+        int num; // 偏移 0
+        long age;// 偏移 8 
+    } student5; //size 16
 
+     struct Student7
+    {
+        //1. 每个成员按照 max（GCC 4，数据成员长度）的倍数进行对齐
+        // 2. 结构体按照min（GCC 4，最大数据成员长度）的倍数进行对齐 
+        long age;// 偏移 8 
+        int num; // 偏移 0
+    } student7; //size 16
+
+    struct Student6
+    {
+        char num; //
+        char age[2];// 偏移 1
+    } student6; //size 3
+
+struct Student2
+    {
+        char name[7];
+    } student2;
+
+      struct Student3
+    {
+        long num; // 按照 4对齐
+        char name[5];// 按照 4*2 对齐
+        short age;// 按照
+        char sex; // 按照 1 对齐
+    } student3; //size 24
+    
+
+    struct Student4
+    {
+        long num; // 偏移 0
+        short age;// 偏移: 8=len(short)*4
+        char name[5];// 偏移： （8 + 2） = len（char）* 10
+        char sex; // 偏移： （8+2 + 5）= 15 = len（char）*15
+    } student4; //size 16
+
+
+    cout << "sizeof long: " << sizeof(long) << endl;
+    cout << "address of student 1  " << &student1 << " sizeof student1 " << sizeof(student1) << endl;
+    cout << "address of student 3  " << &student3 << " sizeof student3 " << sizeof(student3) << endl;
+    cout << "address of student 1 num: " << &student1.num << " name: " << &student1.name 
+    << " age: " << &student1.age ;
+    cout<<" sex: " << &student1.sex <<endl;
+
+    cout << "address of student 2  " << &student2 << " sizeof student2 " << sizeof(student2) << endl;
+    cout << "align value of student2  " << alignof(student2) << endl;
+}
+
+void testStringOp(){
+    ofstream File1("text.txt");
+    string d("20160314");
+    string y = d.substr(0, 4);
+    int k = d.find("2");
+    int i = d.find("3");
+    string m = d.substr(k + 2, i - k);
+    string dd = d.substr(i + 1, 2);
+    string n = dd + m + y;
+    File1 << n << endl;
+    File1.close();
+}
+
+void testMulTable(){
+    int m, n, k;
+    cin >> m >> n >> k;
+    cout << mul(m, n, k) << endl;
+
+}
+
+void testConstant(){
+    const int aa = 10;
+    int *p = (int *)(&aa);
+    *p = 20;                                         // can modify value in the constant varible's memory
+    cout << "a = " << aa << ", *p = " << *p << endl; // but the constant variable doesn't change
+}
+
+void testBitset(){
+    std::bitset<4> b;
+    std::cout << b << "\n";
+    std::cout << b.flip(0) << '\n';
+    std::cout << b.flip(2) << '\n';
+    std::cout << b.flip() << '\n';
+}
+
+void testParentMatch(){
     // [(3+4)*5]/2
     // [(3+4*5]/2
+    string input;
+    cin >> input;
+    string res = match(input) ? "true" : "false";
+    cout << res;
+}
 
-    // int m, n, k;
-    // cin >> m >> n >> k;
-    // cout << mul(m, n, k) << endl;
 
+void testOperPriority(){
+    double a = 1;
+    if (a == 0.0)
+    {
+        cout << "a is" << 0 << endl;
+    }
 
-    // cout << "sizeof long: " << sizeof(long) << endl;
-    // cout << "address of student 1  " << &student1 << " sizeof student1 " << sizeof(student1) << endl;
-    // cout << "address of student 2  " << &student2 << " sizeof student2 " << sizeof(student2) << endl;
-    // cout << "align value of student2  " << alignof(student2) << endl;
+    char a1[14] = "Hello, world!";
+    int i = 3;
+    printf("%d %d\n", ++i, ++i);
 
-    // ofstream File1("text.txt");
-    // string d("20160314");
-    // string y = d.substr(0, 4);
-    // int k = d.find("2");
-    // int i = d.find("3");
-    // string m = d.substr(k + 2, i - k);
-    // string dd = d.substr(i + 1, 2);
-    // string n = dd + m + y;
-    // File1 << n << endl;
-    // File1.close();
+    int t = 3;
+    int x, y;
+    x = y = 2;
+    t = x++ || ++y; // A||B, if A is true, don't check B
+    cout << "t : " << t << "  y: " << y << endl;
 
-    // double a = 1;
-    // if (a == 0.0)
-    // {
-    //     cout << "a is" << 0 << endl;
-    // }
+}
 
-    // char a1[14] = "Hello, world!";
-    // int i = 3;
-    // printf("%d %d\n", ++i, ++i);
-
-    // int t = 3;
-    // int x, y;
-    // x = y = 2;
-    // t = x++ || ++y; // A||B, if A is true, don't check B
-    // cout << "t : " << t << "  y: " << y << endl;
-
-    // const int aa = 10;
-    // int *p = (int *)(&aa);
-    // *p = 20;                                         // can modify value in the constant varible's memory
-    // cout << "a = " << aa << ", *p = " << *p << endl; // but the constant variable doesn't change
-
-    // std::bitset<4> b;
-    // std::cout << b << "\n";
-    // std::cout << b.flip(0) << '\n';
-    // std::cout << b.flip(2) << '\n';
-    // std::cout << b.flip() << '\n';
-
-    // vector<int> arr{0, 1, 2, 3};
-
+int main(int argc, char const *argv[])
+{
+  testAlignMemoryOfStruct();
     return 0;
 }
