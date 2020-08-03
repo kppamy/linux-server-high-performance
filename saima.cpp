d5e0391109b4a09e7d9960f44a4267bc3bee517b
chenay <anyan.chen@technicolor.com>
2020-08-03 10:18:25 +0800

    struct memory align
    +        //1. 每个成员的偏移为自身数据类型长度的整数倍
    +        // 2. 结构体的整体长度为 最大数据成员 类型长度 的整数倍
    +        //  3. 为节省内存，应将结构体按照类型长度从大到小进行排序

--- saima.cpp
+++ saima.cpp
@@ -7,11 +7,9 @@
 #include <utility>
 #include <stack>
 
-
 #include <iostream>
 #include <vector>
 using namespace std;
-
 
 #include <iostream>
 #include <algorithm>
@@ -80,69 +78,67 @@ void testAlignMemoryOfStruct()
 {
     struct Student
     {
-        int num; // 偏移 0
-        char name[7];//偏移 4
-        char sex; // 
-        long age;// 偏移 16
-    } student1; //size 24
+        int num;      // 偏移 0
+        char name[7]; //偏移 4
+        char sex;     //
+        long age;     // 偏移 16
+    } student1;       //size 24
 
-     struct Student5
+    struct Student5
     {
-        //1. 每个成员按照 max（GCC 4，数据成员长度）的倍数进行对齐
-        // 2. 结构体按照min（GCC 4，最大数据成员长度）的倍数进行对齐 
-        int num; // 偏移 0
-        long age;// 偏移 8 
-    } student5; //size 16
+        int num;  // 偏移 0
+        long age; // 偏移 8
+    } student5;   //size 16
 
-     struct Student7
+    struct Student7
     {
-        //1. 每个成员按照 max（GCC 4，数据成员长度）的倍数进行对齐
-        // 2. 结构体按照min（GCC 4，最大数据成员长度）的倍数进行对齐 
-        long age;// 偏移 8 
-        int num; // 偏移 0
-    } student7; //size 16
+        //1. 每个成员的偏移为自身数据类型长度的整数倍
+        // 2. 结构体的整体长度为 最大数据成员 类型长度 的整数倍
+        //  3. 为节省内存，应将结构体按照类型长度从大到小进行排序
+        long age; // 偏移 8
+        int num;  // 偏移 0
+    } student7;   //size 16
 
     struct Student6
     {
-        char num; //
-        char age[2];// 偏移 1
-    } student6; //size 3
+        char num;    //
+        char age[2]; // 偏移 1
+    } student6;      //size 3
 
-struct Student2
+    struct Student2
     {
         char name[7];
     } student2;
 
-      struct Student3
+    struct Student3
     {
-        long num; // 按照 4对齐
-        char name[5];// 按照 4*2 对齐
-        short age;// 按照
-        char sex; // 按照 1 对齐
-    } student3; //size 24
-    
+        long num;     // 按照 4对齐
+        char name[5]; // 按照 4*2 对齐
+        short age;    // 按照
+        char sex;     // 按照 1 对齐
+    } student3;       //size 24
 
     struct Student4
     {
-        long num; // 偏移 0
-        short age;// 偏移: 8=len(short)*4
-        char name[5];// 偏移： （8 + 2） = len（char）* 10
-        char sex; // 偏移： （8+2 + 5）= 15 = len（char）*15
-    } student4; //size 16
-
+        long num;     // 偏移 0
+        short age;    // 偏移: 8=len(short)*4
+        char name[5]; // 偏移： （8 + 2） = len（char）* 10
+        char sex;     // 偏移： （8+2 + 5）= 15 = len（char）*15
+    } student4;       //size 16
 
     cout << "sizeof long: " << sizeof(long) << endl;
     cout << "address of student 1  " << &student1 << " sizeof student1 " << sizeof(student1) << endl;
     cout << "address of student 3  " << &student3 << " sizeof student3 " << sizeof(student3) << endl;
-    cout << "address of student 1 num: " << &student1.num << " name: " << &student1.name 
-    << " age: " << &student1.age ;
-    cout<<" sex: " << &student1.sex <<endl;
+    cout << "address of student 1 num: " << &student1.num << " name: " << &student1.name
+         << " age: " << &student1.age;
+    cout << " sex: " << &student1.sex << endl;
 
     cout << "address of student 2  " << &student2 << " sizeof student2 " << sizeof(student2) << endl;
     cout << "align value of student2  " << alignof(student2) << endl;
 }
 
-void testStringOp(){
+void testStringOp()
+{
     ofstream File1("text.txt");
     string d("20160314");
     string y = d.substr(0, 4);
@@ -155,21 +151,23 @@ void testStringOp(){
     File1.close();
 }
 
-void testMulTable(){
+void testMulTable()
+{
     int m, n, k;
     cin >> m >> n >> k;
     cout << mul(m, n, k) << endl;
-
 }
 
-void testConstant(){
+void testConstant()
+{
     const int aa = 10;
     int *p = (int *)(&aa);
     *p = 20;                                         // can modify value in the constant varible's memory
     cout << "a = " << aa << ", *p = " << *p << endl; // but the constant variable doesn't change
 }
 
-void testBitset(){
+void testBitset()
+{
     std::bitset<4> b;
     std::cout << b << "\n";
     std::cout << b.flip(0) << '\n';
@@ -177,7 +175,8 @@ void testBitset(){
     std::cout << b.flip() << '\n';
 }
 
-void testParentMatch(){
+void testParentMatch()
+{
     // [(3+4)*5]/2
     // [(3+4*5]/2
     string input;
@@ -186,8 +185,8 @@ void testParentMatch(){
     cout << res;
 }
 
-
-void testOperPriority(){
+void testOperPriority()
+{
     double a = 1;
     if (a == 0.0)
     {
@@ -203,11 +202,10 @@ void testOperPriority(){
     x = y = 2;
     t = x++ || ++y; // A||B, if A is true, don't check B
     cout << "t : " << t << "  y: " << y << endl;
-
 }
 
 int main(int argc, char const *argv[])
 {
-  testAlignMemoryOfStruct();
+    testAlignMemoryOfStruct();
     return 0;
 }
