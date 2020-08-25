802f6a1b58a844cd6a0bef0c4c151d45bef21b39
chenay <anyan.chen@technicolor.com>
2020-08-25 17:28:59 +0800

    schema for 627. Swap Salary

--- sqltest.sql
+++ sqltest.sql
@@ -124,3 +124,12 @@ insert into cinema (id, movie, description, rating) values ('3', 'irish', 'borin
 insert into cinema (id, movie, description, rating) values ('4', 'Ice song', 'Fantacy', '8.6');
 insert into cinema (id, movie, description, rating) values ('5', 'House card', 'Interesting', '9.1');
 SELECT * FROM cinema WHERE id % 2 != 0 AND description != 'boring' ORDER BY rating DESC;
+
+create table if not exists salary(id int, name varchar(100), sex char(1), salary int);
+-- Truncate table salary;
+insert into salary (id, name, sex, salary) values ('1', 'A', 'm', '2500');
+insert into salary (id, name, sex, salary) values ('2', 'B', 'f', '1500');
+insert into salary (id, name, sex, salary) values ('3', 'C', 'm', '5500');
+insert into salary (id, name, sex, salary) values ('4', 'D', 'f', '500');
+SELECT * FROM Salary;
+UPDATE salary SET sex = char(Ascii(sex) ^ Ascii('m') ^ Ascii('f'));
