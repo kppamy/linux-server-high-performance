a201b5194d76f30e198f8e669fff9af229c8a27d
chenay <anyan.chen@technicolor.com>
2020-09-18 22:59:40 +0800

    148. Sort List
    quick sort
    
     7.65%

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -48,7 +48,7 @@ ListNode *vectorToCircleListNode(const vector<int> &list, int pos)
   return ptr;
 }
 
-void prettyPrintLinkedList(ListNode *node, int pos)
+void prettyPrintLinkedList(ListNode *node, int pos = 0)
 {
   int counter = -1;
   ListNode *head = node;
@@ -119,7 +119,7 @@ bool LinkedList::hasCycle(ListNode *head)
   return false;
 }
 
-int main()
+void testCycle()
 {
   vector<int> input{1, 2, 3, 4, 5, 6, 7};
   // ListNode* list = vectorToListNode(input);
@@ -134,5 +134,98 @@ int main()
   }
   // ll.deleteNode(list->next->next);
   // prettyPrintLinkedList(ll.removeEndNthNode(list,7));
+}
+
+void dumySwap(ListNode *n1, ListNode *n2)
+{
+  if (n1 == n2)
+    return;
+  int tmp = n1->val;
+  n1->val = n2->val;
+  n2->val = tmp;
+}
+
+pair<ListNode *, int> splitList(ListNode *pivot, int len)
+{
+  // len > 1
+  ListNode *lower = pivot; // for "next" lower node
+  ListNode *higher = pivot->next;
+  int pos = 0;
+  // split
+  while (higher && len > 1)
+  {
+    if (higher->val < pivot->val)
+    {
+      dumySwap(higher, lower->next);
+      lower = lower->next;
+      pos++;
+    }
+    higher = higher->next;
+    len--;
+  }
+  if (pos > 0)
+    dumySwap(lower, pivot);
+  return pair<ListNode *, int>{lower, pos};
+}
+
+ListNode *qSortList(ListNode *head, int len)
+{
+  pair<ListNode *, int> pa = splitList(head, len);
+  int spos = pa.second;
+  ListNode *snode = pa.first;
+  if (spos > 1)
+    qSortList(head, spos);
+  if (len - spos - 1 > 1 && snode->next)
+    qSortList(snode->next, len - spos - 1);
+  return head;
+}
+
+// 148. Sort List
+// Runtime: 460 ms, faster than 7.65% of C++ online submissions for Sort List.
+// Memory Usage: 16.2 MB, less than 39.73% of C++ online submissions for Sort Lis
+ListNode *sortList(ListNode *head)
+{
+  if (!head)
+    return nullptr;
+  int sz = 0;
+  ListNode *tmp = head;
+  while (tmp)
+  {
+    sz++;
+    tmp = tmp->next;
+  }
+  //compute sz
+  if (sz == 1)
+    return head;
+  return qSortList(head, sz);
+}
+
+#include "common.h"
+void testsortList()
+{
+  my2arr cases = {
+      {1, 3, 2},
+      {1, 2},
+      {2, 1},
+      {4, 2, 1, 3},
+      {-1, 5, 3, 4, 0},
+      {},
+  };
+
+  for (auto &&item : cases)
+  {
+    ListNode *head = vectorToListNode(item);
+    prettyPrintLinkedList(head);
+    sortList(head);
+    cout << "sorted list: " << endl;
+    prettyPrintLinkedList(head);
+    cout << endl;
+  }
+}
+
+int main()
+{
+
+  testsortList();
   return 0;
 }
