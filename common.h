d025d661b52e0dd915569680ee6bc7171fe197a7
chenay <anyan.chen@technicolor.com>
2020-09-23 12:49:46 +0800

    // 155. Min Stack
    1. stl::min couldn't be used for vector iterators, should use
    min_element instead
    2. when use some value for invalid values, think about INT_MAX,
        which is different than INT8_MAX
    3. when use vector as the storage, and don't give size at the first,
    
        could use push_back, instead of array index. however, keep in
        mind if you need to use pop_back as the counterpart operations
    
    overloaded output operators, smart print when do operations which could
    change the data

--- common.h
+++ common.h
@@ -60,3 +60,97 @@ vector<int> generateArray(int num, int max)
     }
     return arr;
 }
+
+template<typename f, typename u, typename t>
+void format_test(f func, u inputf, t print){
+ auto&& cases=u();
+ for(auto&& item :cases){
+     func(item);
+     print(item);
+ }
+}
+
+#include <iostream>
+#include <string>
+#include <map>
+
+ 
+#define DECLARE_CLASS() \
+    protected: \
+        static ClassInfo ms_classinfo; \
+    public:  \
+        static Object* CreateObject();  
+ 
+#define IMPLEMENT_CLASS(interface_name, class_name)            \
+	ClassInfo class_name::ms_classinfo(interface_name,(ObjectConstructorFn)class_name::CreateObject);\
+    Object* class_name::CreateObject() \
+        { return new class_name;} 
+ 
+class ClassInfo;
+class Object;
+typedef Object* (*ObjectConstructorFn)();
+ 
+class Object  
+{ 
+protected:
+    Object(){}
+public:  
+    virtual ~Object(){}
+    static void Register(ClassInfo* ci);         
+    static Object* CreateObject(std::string name);     
+    static std::map<std::string, ClassInfo *> *classInfoMap;
+};  
+ 
+class ClassInfo  
+{  
+public:  
+	ClassInfo(const std::string className, ObjectConstructorFn ctor);
+	ClassInfo();
+	Object *CreateObject()const;
+ 
+public:  
+	std::string m_className;
+	ObjectConstructorFn m_objectConstructor;
+}; 
+ 
+std::map<std::string, ClassInfo *> *Object::classInfoMap = new std::map<std::string, ClassInfo*>();
+ 
+void Object::Register(ClassInfo* ci)  
+{   
+	if (NULL != ci && classInfoMap->find(ci->m_className) == classInfoMap->end())
+	{  
+		classInfoMap->insert(std::map<std::string, ClassInfo*>::value_type(ci->m_className, ci)); 
+	}  
+}
+ 
+Object* Object::CreateObject(std::string name)  
+{
+	std::map<std::string, ClassInfo*>::const_iterator iter = classInfoMap->find(name);  
+	if (iter != classInfoMap->end())
+	{
+		return iter->second->CreateObject();
+	}
+	return NULL;  
+}  
+ 
+ClassInfo::ClassInfo(const std::string className, ObjectConstructorFn ctor):m_className(className), m_objectConstructor(ctor)  
+{  
+	Object::Register(this);
+}
+ 
+ClassInfo::ClassInfo()
+{
+}
+ 
+Object *ClassInfo::CreateObject()const 
+{ 
+	return m_objectConstructor ? (*m_objectConstructor)() : 0; 
+}
+ 
+class Test:public Object
+{
+        DECLARE_CLASS()
+public:
+        Test(){cout << "Test constructor" << endl;}
+        ~Test(){cout << "Test destructor" << endl;}
+};
--- lstack.cpp
+++ lstack.cpp
@@ -27,7 +27,7 @@ bool isValid(string s)
         }
         else
         {
-            if(data.size()==0)
+            if (data.size() == 0)
                 return false;
             top = data.top();
             data.pop();
@@ -37,24 +37,259 @@ bool isValid(string s)
             }
         }
     }
-    if(data.size()==0)
+    if (data.size() == 0)
         return true;
     return false;
 }
 
-#include<iostream>
-void testsValid(){
-    cout<<"()"<<" is "<<(isValid("()")?"":" not ")<<" valid Parentheses"<<endl;
-    cout<<"(("<<" is "<<(isValid("((")?"":" not ")<<" valid Parentheses"<<endl;
-    cout<<"()[]{}"<<" is "<<(isValid("()[]{}")?"":" not ")<<" valid Parentheses"<<endl;
-    cout<<"(]"<<" is "<<(isValid("(]")?"":" not ")<<" valid Parentheses"<<endl;
-    cout<<"([)]"<<" is "<<(isValid("([)]")?"":" not ")<<" valid Parentheses"<<endl;
-    cout<<"{[]}"<<" is "<<(isValid("{[]}")?"":" not ")<<" valid Parentheses"<<endl;
+#include <iostream>
+void testsValid()
+{
+    cout << "()"
+         << " is " << (isValid("()") ? "" : " not ") << " valid Parentheses" << endl;
+    cout << "(("
+         << " is " << (isValid("((") ? "" : " not ") << " valid Parentheses" << endl;
+    cout << "()[]{}"
+         << " is " << (isValid("()[]{}") ? "" : " not ") << " valid Parentheses" << endl;
+    cout << "(]"
+         << " is " << (isValid("(]") ? "" : " not ") << " valid Parentheses" << endl;
+    cout << "([)]"
+         << " is " << (isValid("([)]") ? "" : " not ") << " valid Parentheses" << endl;
+    cout << "{[]}"
+         << " is " << (isValid("{[]}") ? "" : " not ") << " valid Parentheses" << endl;
+}
+
+#include <vector>
+#include <algorithm>
+#include "common.h"
+
+// 155. Min Stack
+// Runtime: 40 ms, faster than 95.85% of C++ online submissions for Min Stack.
+// Memory Usage: 16 MB, less than 94.24% of C++ online submissions for Min Stack.
+class MinStack : public Object
+{
+    DECLARE_CLASS();
+
+private:
+    vector<int> data;
+    int rear;
+    int smallest;
+    int size;
+
+public:
+    /** initialize your data structure here. */
+    MinStack()
+    {
+        rear = -1;
+        size = 0;
+        smallest = INT_MAX;
+    }
+
+    friend ostream &operator<<(ostream &os, const MinStack &ms)
+    {
+        if (ms.size == 0)
+            return os;
+        for (int i = ms.rear - ms.size + 1; i <= ms.rear; i++)
+            os << ms.data[i] << " ";
+        return os;
+    }
+
+    bool isEmpty()
+    {
+        return (size == 0);
+    }
+
+    void push(int x)
+    {
+        // if use push_back,keep in mind that if we need to use the pop_back as well
+        data.push_back(x);
+        size++;
+        rear++;
+        smallest = (x < smallest) ? x : smallest;
+        cout << __func__ << " minStack: " << *this << endl;
+    }
+
+    void pop()
+    {
+        if (isEmpty())
+            return;
+        rear--;
+        size--;
+        // when use vector to simulate the stack/queue, it's better to keep the storage the same
+        // as the pointers, not just move the pointers aroud
+        data.pop_back();
+        cout << __func__ << " minStack: " << *this << endl;
+        if (size == 0)
+        {
+            smallest = INT_MAX;
+            data.resize(0);
+        }
+        else
+        {
+            // std::min couldn't iterator over vector's iterator range.
+            // there is no such overloaded function, however, the compile doesn't show warning,
+            // as it convert the iterator (int*) to int implictly.
+            smallest = *min_element(data.begin(), data.begin() + size);
+        }
+    }
+
+    int top()
+    {
+        if (!isEmpty())
+            return data[rear];
+        // must return some value or the leetcode compile will raise compiling error
+        return INT_MAX;
+    }
+
+    int getMin()
+    {
+        if (!isEmpty())
+            return smallest;
+        // must return some value or the leetcode compile will raise compiling error
+        return INT_MAX;
+    }
+};
+
+IMPLEMENT_CLASS("MinStack", MinStack);
+
+void formatCall()
+{
+    MinStack *obj = dynamic_cast<MinStack *>(Object::CreateObject("MinStack"));
+    obj->push(1);
+    cout << *obj;
+    delete obj;
+}
+
+void testMinStack()
+{
+    MinStack *minStack = new MinStack();
+    minStack->push(0);
+    minStack->pop();
+
+    minStack->push(-2);
+    minStack->push(0);
+    minStack->push(-3);
+    cout << "minimum: " << minStack->getMin() << endl;
+
+    minStack->pop();
+    cout << "minStack->top() " << minStack->top() << endl; // return 0
+    cout << "minimum: " << minStack->getMin() << endl;
+    minStack->pop();
+    cout << "minStack->top() " << minStack->top() << endl; // return 0
+    minStack->pop();
+    cout << "minStack->top() " << minStack->top() << endl; // return 0
+    minStack->pop();
+    cout << "minStack->top() " << minStack->top() << endl; // return 0
+
+    // minStack->getMin(); // return -2
+    delete minStack;
+}
+
+void testMinStack2()
+{
+    MinStack *minStack = new MinStack();
+    minStack->push(2);
+    minStack->push(0);
+    minStack->push(3);
+    minStack->push(0);
+    cout << "minimum: " << minStack->getMin() << endl;
+
+    minStack->pop();
+    cout << "minimum: " << minStack->getMin() << endl;
+
+    minStack->pop();
+    cout << "minimum: " << minStack->getMin() << endl;
+
+    minStack->pop();
+    cout << "minimum: " << minStack->getMin() << endl;
+
+    minStack->pop();
+    minStack->push(0);
+
+    delete minStack;
+}
+
+void testMinStack3()
+{
+    //     ["MinStack","push","push","push","top","pop","getMin",
+    //     [[],[2147483646],[2147483646],[2147483647],[],[],[],
+
+    MinStack *minStack = new MinStack();
+    minStack->push(2147483646);
+    minStack->push(2147483646);
+    minStack->push(2147483647);
+    cout << "top: " << minStack->top() << endl;
+    minStack->pop();
+    cout << "minimum: " << minStack->getMin() << endl;
+
+    // "pop","getMin","pop",
+    // [],[],[],
+    minStack->pop();
+    cout << "minimum: " << minStack->getMin() << endl;
+    minStack->pop();
+
+
+    // "push","top","getMin", "push","top","getMin","pop","getMin"]
+    // [2147483647],[],[],[-2147483648],[],[],[],[]]
+
+    minStack->push(2147483647);
+    cout << "top: " << minStack->top() << endl;
+    cout << "minimum: " << minStack->getMin() << endl;
+    minStack->push(-2147483648);
+    cout << "top: " << minStack->top() << endl;
+    cout << "minimum: " << minStack->getMin() << endl;
+
+    minStack->pop();
+    cout << "minStack: " << *minStack << endl;
+    cout << "minimum: " << minStack->getMin() << endl;
+
+    delete minStack;
+}
+
+void testMinStack4()
+{
+    //     ["MinStack","push","push","getMin","getMin",
+    //     [[],[-10],[14],[],[],
+
+    MinStack *minStack = new MinStack();
+    minStack->push(-10);
+    minStack->push(14);
+    cout << "minimum: " << minStack->getMin() << endl;
+    cout << "minimum: " << minStack->getMin() << endl;
+
+    // "push","getMin","getMin",
+    // [-20],[],[],
+    minStack->push(-20);
+    cout << "minimum: " << minStack->getMin() << endl;
+    cout << "minimum: " << minStack->getMin() << endl;
+
+    // "top","getMin","pop",
+    // [],[],[],
+    cout << "top: " << minStack->top() << endl;
+    cout << "minimum: " << minStack->getMin() << endl;
+    minStack->pop();
+
+    // "push","push","getMin",
+    // [10],[-7],[],
+    minStack->push(10);
+    minStack->push(-7);
+    cout << "minimum: " << minStack->getMin() << endl;
+
+    // "push","pop","top","getMin","pop"]
+    // [-7],[],[],[],[]]
+    minStack->push(-7);
+    minStack->pop();
+    cout << "top: " << minStack->top() << endl;
+    cout << "minimum: " << minStack->getMin() << endl;
+    minStack->pop();
 }
 
 int main(int argc, char const *argv[])
 {
-    testsValid();
+    // testsValid();
+    // testMinStack2();
+    testMinStack3();
+    // testMinStack4();
+    // formatCall();
     return 0;
 }
-
