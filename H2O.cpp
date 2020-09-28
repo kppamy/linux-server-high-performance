19aa4e2a1ec119953fccd4f1bc03fbf7e1765fb5
chenay <anyan.chen@technicolor.com>
2020-09-28 11:40:06 +0800

    1117. Building H2O
    break down:
    
    H threads:
        synchronization requirement: synchronize with O thread, and also
        need to synchronize with itself.
        when 2 H have been synchronized,H threads should block for O
        threads, and should block the 3rd H thread as well.
    
    Assume multiple threads comes at the same time, then what?
    
    be careful when use join if want to fire multiple threads in main thread,
    because if one thread get blocked, then the rest of the threads don't
    have chance to get fired at all.

--- H2O.cpp
+++ H2O.cpp
@@ -6,37 +6,33 @@
 #include "common.h"
 #include <vector>
 using namespace std;
+// 1117. Building H2O
+// Runtime: 212 ms, faster than 46.32% of C++ online submissions for Building H2O.
+// Memory Usage: 9 MB, less than 53.41% of C++ online submissions for Building H2O.
 class H2O
 {
 private:
     atomic<int> hyd{0};
     mutex hlock;
     mutex olock;
-    vector<long> otime;
-    vector<long> htime;
-
 public:
     H2O()
     {
-        hlock.lock();
     }
 
     void hydrogen(function<void()> releaseHydrogen)
     {
-
-        if (hyd == 2) // (1)
-        {
-            cout << " ....  ";
-            hlock.lock();
-        }
+        hlock.lock();
         // releaseHydrogen() outputs "H". Do not change or remove this line.
-        // htime.push_back(clock());
         releaseHydrogen();
         hyd++;
-        if (hyd == 2)
+        if (hyd == 2) // (1)
         {
-            cout << " ..2..  ";
             olock.unlock();
+        }
+        else if (hyd < 2)
+        {
+            hlock.unlock();
         }
     }
 
@@ -44,70 +40,54 @@ public:
     {
         olock.lock();
         // releaseOxygen() outputs "O". Do not change or remove this line.
-        // otime.push_back(clock());
         releaseOxygen();
         if (hyd == 2)
         {
-            // wait for H
             hyd = 0;
-            cout << " ****  ";
             hlock.unlock();
         }
     }
 };
 
-#include <unistd.h>
-mutex mut;
 void releaseHydrogen()
 {
-    // mut.lock();
     cout << "H";
-    // mut.unlock();
 }
 
 void releaseOxygen()
 {
-    // mut.lock();
     cout << "O";
-    // mut.unlock();
-}
-
-void testUnlockAfterThreadQuit()
-{
-    thread t1(releaseHydrogen);
-    t1.join();
-    thread t2(releaseOxygen);
-    t2.join();
 }
 
 void testH2O()
 {
     H2O ho;
-    string arr = "OOHHHH";
-    int n = 10;
+    // string arr = "HOH";
+    // string arr = "OOHHHH";
+    string arr = "HOHOHOHHH";
+    int n = 100;
     while (n > 0)
     {
-        thread oo(bind(&H2O::oxygen, &ho, releaseOxygen));
-        thread oo2(bind(&H2O::oxygen, &ho, releaseOxygen));
-        thread hh(bind(&H2O::hydrogen, &ho, releaseHydrogen));
-        thread hh2(bind(&H2O::hydrogen, &ho, releaseHydrogen));
-        thread hh3(bind(&H2O::hydrogen, &ho, releaseHydrogen));
-        thread hh4(bind(&H2O::hydrogen, &ho, releaseHydrogen));
-        oo.detach();
-        oo2.detach();
-        hh2.detach();
-        hh.detach();
-        hh3.detach();
-        hh4.detach();
+        for (char c : arr)
+        {
+            if (c == 'H')
+            {
+                thread hh(bind(&H2O::hydrogen, &ho, releaseHydrogen));
+                hh.detach();
+            }
+            else if (c == 'O')
+            {
+                thread oo(bind(&H2O::oxygen, &ho, releaseOxygen));
+                oo.detach();
+            }
+        }
         cout << endl;
         n--;
-        sleep(1);
     }
 }
 
 int main(int argc, char const *argv[])
 {
     testH2O();
-    // testUnlockAfterThreadQuit();
     return 0;
 }
