1bb195da95164bee2d29536181fb92a26147bbf4
chenay <anyan.chen@technicolor.com>
2020-10-01 08:57:21 +0800

    // 944. Delete Columns to Make Sorted

--- greedy.cpp
+++ greedy.cpp
@@ -38,27 +38,73 @@ void testnumWaterBottles()
 // Memory Usage: 6.5 MB, less than 5.09% of C++ online submissions for Split a String in Balanced Strings.
 int balancedStringSplit(string s)
 {
-    int counter=0;
-    int sum=0;
-    for(auto&& c:s){
-        sum+=(c=='L'?1:-1);
-        if(sum==0)
+    int counter = 0;
+    int sum = 0;
+    for (auto &&c : s)
+    {
+        sum += (c == 'L' ? 1 : -1);
+        if (sum == 0)
             counter++;
     }
     return counter;
 }
 
-void testbalancedStringSpli(){
-    cout<<" balancedStringSplit "<<" RLRRLLRLRL " <<balancedStringSplit("RLRRLLRLRL")<<endl;
-    cout<<" balancedStringSplit "<<" RLLLLRRRLR " <<balancedStringSplit("RLLLLRRRLR")<<endl;
-    cout<<" balancedStringSplit "<<" LLLLRRRR " <<balancedStringSplit("LLLLRRRR")<<endl;
-    cout<<" balancedStringSplit "<<" RLRRRLLRLL "<<balancedStringSplit("RLRRRLLRLL")<<endl;
+void testbalancedStringSpli()
+{
+    cout << " balancedStringSplit "
+         << " RLRRLLRLRL " << balancedStringSplit("RLRRLLRLRL") << endl;
+    cout << " balancedStringSplit "
+         << " RLLLLRRRLR " << balancedStringSplit("RLLLLRRRLR") << endl;
+    cout << " balancedStringSplit "
+         << " LLLLRRRR " << balancedStringSplit("LLLLRRRR") << endl;
+    cout << " balancedStringSplit "
+         << " RLRRRLLRLL " << balancedStringSplit("RLRRRLLRLL") << endl;
+}
 
+// 944. Delete Columns to Make Sorted
+// Runtime: 52 ms, faster than 94.37% of C++ online submissions for Delete Columns to Make Sorted.
+// Memory Usage: 12.5 MB, less than 12.60% of C++ online submissions for Delete Columns to Make Sorted.
+int minDeletionSize(vector<string> &A)
+{
+
+    int len = A.size();
+    int wid = A[0].size();
+    auto sorted = [&](int i) -> bool {
+        for (int j = 0; j < len - 1; j++)
+        {
+            if (A[j][i] > A[j + 1][i])
+                return false;
+        }
+        return true;
+    };
+    int del = 0;
+    for (int i = 0; i < wid; i++)
+    {
+        if (!sorted(i))
+            del++;
+    }
+    return del;
+}
+
+void testminDeletionSize()
+{
+    vector<vector<string>> cases = {
+        {"cba", "daf", "ghi"},
+        {"a", "b"},
+        {"zyx", "wvu", "tsr"}};
+    for (auto &&cs : cases)
+    {
+        for (auto &&item : cs)
+            cout << item << ", ";
+        cout << endl;
+        cout << "minDeletionSize: " << minDeletionSize(cs) << endl;
+    }
 }
 
 int main(int argc, char const *argv[])
 {
     // testnumWaterBottles();
-    testbalancedStringSpli();
+    // testbalancedStringSpli();
+    testminDeletionSize();
     return 0;
 }
