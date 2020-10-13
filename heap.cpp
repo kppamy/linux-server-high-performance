230b3763cae58337e494bd4f8653550f06a2c173
chenay <anyan.chen@technicolor.com>
2020-10-13 15:37:19 +0800

    n*(stops+1)*2 storage

--- heap.cpp
+++ heap.cpp
@@ -1,4 +1,5 @@
 #include "common.h"
+#include <iostream>
 using namespace std;
 
 // 703. Kth Largest Element in a Stream
@@ -193,10 +194,10 @@ int lastStoneWeight(vector<int> &stones)
     return heap.top();
 }
 
-#include <array>
-#include <unordered_map>
 // bellman-ford shortest path
 // 787. Cheapest Flights Within K Stops
+// Runtime: 280 ms, faster than 5.92% of C++ online submissions for Cheapest Flights Within K Stops.
+// Memory Usage: 11.7 MB, less than 9.08% of C++ online submissions for Cheapest Flights Within K Stops.
 int findCheapestPrice(int n, vector<vector<int>> &flights, int src, int dst, int K)
 {
     vector<vector<int>> map(n, vector<int>(n, 0));
@@ -206,24 +207,32 @@ int findCheapestPrice(int n, vector<vector<int>> &flights, int src, int dst, int
         map[val[0]][val[1]] = val[2];
         indegree[val[1]]++;
     }
-    typedef vector<array<int, 2>> pathtype;
-    pathtype paths;
-    vector<pathtype> cheapest(n, paths); //n*[(prices,0 stops),(prices 2,1 stops)... ]
-    cheapest[src].push_back({0, -1});
+    vector<int> paths(K + 1, INT_MAX);
+    vector<vector<int>> cheapest(n, paths);
     queue<int> bfs;
     auto relax = [&](int u) {
         for (int v = 0; v < n; v++)
         {
             if (map[u][v] == 0)
                 continue;
-            if (indegree[v] > 0)
+            if (v != src && indegree[v] > 0)
             {
                 bfs.push(v);
                 indegree[v]--;
             }
-            for (auto &&path : cheapest[u])
+            if (u == src)
             {
-                cheapest[v].push_back({path[0] + map[u][v], path[1] + 1});
+                cheapest[v][0] = map[u][v];
+                continue;
+            }
+            for (int i = 0; i < K; i++)
+            {
+                if (cheapest[u][i] == INT_MAX)
+                    continue;
+                if (cheapest[u][i] + map[u][v] < cheapest[v][i + 1])
+                {
+                    cheapest[v][i + 1] = cheapest[u][i] + map[u][v];
+                }
             }
         }
     };
@@ -235,15 +244,12 @@ int findCheapestPrice(int n, vector<vector<int>> &flights, int src, int dst, int
         bfs.pop();
         relax(nd);
     }
-
-    if (cheapest[dst].empty())
-        return -1;
     int mini = INT_MAX;
-    for (auto &&path : cheapest[dst])
+    for (int val : cheapest[dst])
     {
-        if (path[0] < mini && path[1] <= K)
+        if (val < mini)
         {
-            mini = path[0];
+            mini = val;
         }
     }
     return (mini == INT_MAX) ? -1 : mini;
@@ -255,6 +261,10 @@ void testfindCheapestPrice()
     vector<vector<int>> edges = {{0, 1, 100}, {1, 2, 100}, {0, 2, 500}};
     cout << "from " << src << " to " << dst << " within " << k << " stops, cheapest: " << findCheapestPrice(n, edges, src, dst, k) << endl;
     k = 0;
+    cout << "from " << src << " to " << dst << " within " << k << " stops, cheapest: " << findCheapestPrice(n, edges, src, dst, k) << endl;
+
+    edges = {{0, 12, 28}, {5, 6, 39}, {8, 6, 59}, {13, 15, 7}, {13, 12, 38}, {10, 12, 35}, {15, 3, 23}, {7, 11, 26}, {9, 4, 65}, {10, 2, 38}, {4, 7, 7}, {14, 15, 31}, {2, 12, 44}, {8, 10, 34}, {13, 6, 29}, {5, 14, 89}, {11, 16, 13}, {7, 3, 46}, {10, 15, 19}, {12, 4, 58}, {13, 16, 11}, {16, 4, 76}, {2, 0, 12}, {15, 0, 22}, {16, 12, 13}, {7, 1, 29}, {7, 14, 100}, {16, 1, 14}, {9, 6, 74}, {11, 1, 73}, {2, 11, 60}, {10, 11, 85}, {2, 5, 49}, {3, 4, 17}, {4, 9, 77}, {16, 3, 47}, {15, 6, 78}, {14, 1, 90}, {10, 5, 95}, {1, 11, 30}, {11, 0, 37}, {10, 4, 86}, {0, 8, 57}, {6, 14, 68}, {16, 8, 3}, {13, 0, 65}, {2, 13, 6}, {5, 13, 5}, {8, 11, 31}, {6, 10, 20}, {6, 2, 33}, {9, 1, 3}, {14, 9, 58}, {12, 3, 19}, {11, 2, 74}, {12, 14, 48}, {16, 11, 100}, {3, 12, 38}, {12, 13, 77}, {10, 9, 99}, {15, 13, 98}, {15, 12, 71}, {1, 4, 28}, {7, 0, 83}, {3, 5, 100}, {8, 9, 14}, {15, 11, 57}, {3, 6, 65}, {1, 3, 45}, {14, 7, 74}, {2, 10, 39}, {4, 8, 73}, {13, 5, 77}, {10, 0, 43}, {12, 9, 92}, {8, 2, 26}, {1, 7, 7}, {9, 12, 10}, {13, 11, 64}, {8, 13, 80}, {6, 12, 74}, {9, 7, 35}, {0, 15, 48}, {3, 7, 87}, {16, 9, 42}, {5, 16, 64}, {4, 5, 65}, {15, 14, 70}, {12, 0, 13}, {16, 14, 52}, {3, 10, 80}, {14, 11, 85}, {15, 2, 77}, {4, 11, 19}, {2, 7, 49}, {10, 7, 78}, {14, 6, 84}, {13, 7, 50}, {11, 6, 75}, {5, 10, 46}, {13, 8, 43}, {9, 10, 49}, {7, 12, 64}, {0, 10, 76}, {5, 9, 77}, {8, 3, 28}, {11, 9, 28}, {12, 16, 87}, {12, 6, 24}, {9, 15, 94}, {5, 7, 77}, {4, 10, 18}, {7, 2, 11}, {9, 5, 41}};
+    n = 17, src = 13, dst = 4, k = 13;
     cout << "from " << src << " to " << dst << " within " << k << " stops, cheapest: " << findCheapestPrice(n, edges, src, dst, k) << endl;
 
     // Time Limit Exceeded
