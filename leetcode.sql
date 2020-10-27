de99f154041cf16362fdeb776932170a56e333ed
chenay <anyan.chen@technicolor.com>
2020-10-27 10:11:00 +0800

    185. Department Top Three Salaries
    use order by on multiple columns,
    use dense_rank with partion by + order by

--- leetcode.sql
+++ leetcode.sql
@@ -173,6 +173,31 @@ SELECT score, dense_RANK() OVER (ORDER BY score DESC) AS `Rank`
 FROM scores
 
 -- 185. Department Top Three Salaries
+Create table If Not Exists Employee (Id int, Name varchar(255), Salary int, DepartmentId int) ;
+Create table If Not Exists Department (Id int, Name varchar(255)) ;
+Truncate table Employee ;
+insert into Employee (Id, Name, Salary, DepartmentId) values ('1', 'Joe', '85000', '1') ;
+insert into Employee (Id, Name, Salary, DepartmentId) values ('2', 'Henry', '80000', '2') ;
+insert into Employee (Id, Name, Salary, DepartmentId) values ('3', 'Sam', '60000', '2') ;
+insert into Employee (Id, Name, Salary, DepartmentId) values ('4', 'Max', '90000', '1') ;
+insert into Employee (Id, Name, Salary, DepartmentId) values ('5', 'Janet', '69000', '1') ;
+insert into Employee (Id, Name, Salary, DepartmentId) values ('6', 'Randy', '85000', '1') ;
+insert into Employee (Id, Name, Salary, DepartmentId) values ('7', 'Will', '70000', '1') ;
+Truncate table Department ;
+insert into Department (Id, Name) values ('1', 'IT') ;
+insert into Department (Id, Name) values ('2', 'Sales') ;
+
+SELECT * FROM Employee;
+SELECT * FROM Department;
+
+
+-- 1137 ms, faster than 22.54% 
+SELECT Department, Employee, Salary 
+FROM 
+    (SELECT d.name as Department, e.name as Employee, Salary ,DENSE_RANK() OVER(Partition BY d.name ORDER BY d.name,Salary DESC) AS ranks
+     FROM Employee e join Department d on e.departmentid=d.id) AS n 
+WHERE ranks<=3;
+
 
 -- 601. Human Traffic of Stadium
 
