45e81121cc11c46f66e80368a22351bed46ea770
chenay <anyan.chen@technicolor.com>
2020-10-28 08:51:07 +0800

    620. Not Boring Movies
    mod(a,2) is faster than a%2
    while no significant difference between <> and !=

--- sqltest.sql
+++ sqltest.sql
@@ -117,13 +117,18 @@ SELECT  student FROM courses GROUP BY class HAVING count( student)>1 ;
 
 -- 620. Not Boring Movies
 Create table If Not Exists cinema (id int, movie varchar(255), description varchar(255), rating float(2, 1));
--- Truncate table cinema;
+Truncate table cinema;
 insert into cinema (id, movie, description, rating) values ('1', 'War', 'great 3D', '8.9');
 insert into cinema (id, movie, description, rating) values ('2', 'Science', 'fiction', '8.5');
 insert into cinema (id, movie, description, rating) values ('3', 'irish', 'boring', '6.2');
 insert into cinema (id, movie, description, rating) values ('4', 'Ice song', 'Fantacy', '8.6');
 insert into cinema (id, movie, description, rating) values ('5', 'House card', 'Interesting', '9.1');
+
+SELECT * FROM cinema;
+-- 240 ms, faster than 22.96% o
 SELECT * FROM cinema WHERE id % 2 != 0 AND description != 'boring' ORDER BY rating DESC;
+-- 172 ms, faster than 91.30% 
+SELECT * FROM cinema WHERE MOD(id,2)=1 AND description <> 'boring' ORDER BY rating DESC;
 
 
 -- 627. Swap Salary
