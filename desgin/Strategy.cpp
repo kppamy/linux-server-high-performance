294eb89bf93e9065dea4961feb348a3d8d45fab4
chenay <anyan.chen@technicolor.com>
2020-12-09 09:17:16 +0800

    design pattern --strategy

---
+++ desgin/Strategy.cpp
@@ -0,0 +1,51 @@
+
+#include <iostream>
+#include <memory>
+using namespace std;
+class Strategy
+{
+public:
+    virtual ~Strategy()
+    {
+        cout << "father delete" << endl;
+    }
+    virtual void implement()
+    {
+        cout << "father strategy" << endl;
+    }
+};
+
+class StrategyA : public Strategy
+{
+public:
+    ~StrategyA()
+    {
+        cout << "childA delete" << endl;
+    }
+    void implement() override
+    {
+        cout << "child A strategy" << endl;
+    }
+};
+
+class Context
+{
+private:
+    unique_ptr<Strategy> sta;
+
+public:
+    Context(unique_ptr<Strategy> s) : sta(move(s)) {}
+    void algo()
+    {
+        sta->implement();
+    }
+};
+
+int main(int argc, char const *argv[])
+{
+    Context c(make_unique<Strategy>());
+    c.algo();
+    Context c1(make_unique<StrategyA>());
+    c1.algo();
+    return 0;
+}
---
+++ desgin/strategy.py
@@ -0,0 +1,25 @@
+import types
+class Strategy:
+    def __init__(self,func=None):
+        self.name = 'stub'
+        if func is not None:
+            self.execute = types.MethodType(func,self)
+
+    def execute(self):
+        print(self.name)
+
+def callcoco(self):
+    print(self.name + "coco")
+
+def callcay(self):
+    print(self.name + "cay")
+
+if __name__ == '__main__':
+    s = Strategy()
+    s.execute()
+    cay=Strategy(callcay)
+    cay.name = 'cay'
+    cay.execute()
+    coco=Strategy(callcoco)
+    coco.name='coco'
+    coco.execute()
