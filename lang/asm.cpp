95c81d8deadad113e25465f68b4c7ea5327f8ed6
chenay <anyan.chen@technicolor.com>
2021-01-05 17:41:35 +0800

    begin mmap test

--- lang/asm.cpp
+++ lang/asm.cpp
@@ -1,16 +1,19 @@
-#include<string>
-#include<iostream>
+#include <string>
+#include <iostream>
+#include <stdio.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <unistd.h>
 using namespace std;
 
-void test(string&& rv){
-	string lc=move(rv);
-    cout<<"address of local rvalue: "<<&lc<<" "<<lc<<endl;
-	
+void test(string &&rv)
+{
+    string lc = move(rv);
+    cout << "address of local rvalue: " << &lc << " " << lc << endl;
 }
 
-string gs="cococco";
-const string gcs="cococcocc";
-
+string gs = "cococco";
+const string gcs = "cococcocc";
 
 class Rvtest
 {
@@ -43,29 +46,61 @@ void testRvalue()
     std::cout << "after return " << r3.i << " " << &r3.i << std::endl;
 }
 
-int main() {
-    cout<<"address of global string: "<<&gs<<" "<<gs<<endl;
-    cout<<"address of global const string: "<<&gcs<<" "<<gcs<<endl;
+void testmmap()
+{
+    size_t pagesize = getpagesize();
 
-    int* arr=new int[2];
-    cout<<"new array address:     "<<arr<<endl;
+    printf("System page size: %zu bytes\n", pagesize);
 
-    std::string lv="sdjfdk";
-    test(lv+lv);
+    char *region = mmap(
+        (void *)(pagesize * (1 << 20)), // Map from the start of the 2^20th page
+        pagesize,                       // for one page length
+        PROT_READ | PROT_WRITE | PROT_EXEC,
+        MAP_ANON | MAP_PRIVATE, // to a private block of hardware memory
+        0,
+        0);
+    if (region == MAP_FAILED)
+    {
+        perror("Could not mmap");
+    }
 
-    const string cs="caycay";  
-    cout<<"address of local const string: "<<&cs<<" "<<cs<<endl;
+    strcpy(region, "Hello, poftut.com");
 
-    cout<<"address of lvalue: "<<&lv<<" "<<lv<<endl;
-    std::string lv2=lv+lv;
-    cout<<"address of lvalue2: "<<&lv2<<" "<<lv2<<endl;
-    std::string&& rv="sdjfdk";
-    string&& rv2=lv+lv;
-    cout<<"address of rvalue: "<<&rv<<" "<<rv<<endl;
-    cout<<"address of rvalue2: "<<&rv2<<" "<<rv2<<endl;
-    cout<<"address of function :%p "<<test<<endl;
+    printf("Contents of region: %s\n", region);
+
+    int unmap_result = munmap(region, 1 << 10);
+    if (unmap_result != 0)
+    {
+        perror("Could not munmap");
+        return 1;
+    }
+}
+
+int main()
+{
+    cout << "address of global string: " << &gs << " " << gs << endl;
+    cout << "address of global const string: " << &gcs << " " << gcs << endl;
+
+    int *arr = new int[2];
+    cout << "new array address:     " << arr << endl;
+
+    std::string lv = "sdjfdk";
+    test(lv + lv);
+
+    const string cs = "caycay";
+    cout << "address of local const string: " << &cs << " " << cs << endl;
+
+    cout << "address of lvalue: " << &lv << " " << lv << endl;
+    std::string lv2 = lv + lv;
+    cout << "address of lvalue2: " << &lv2 << " " << lv2 << endl;
+    std::string &&rv = "sdjfdk";
+    string &&rv2 = lv + lv;
+    cout << "address of rvalue: " << &rv << " " << rv << endl;
+    cout << "address of rvalue2: " << &rv2 << " " << rv2 << endl;
+    cout << "address of function :%p " << test << endl;
 
     testRvalue();
+    testmmap();
 
     getchar();
     return 0;
