8a438909d424fa0d23133f11d3744021278f0db6
chenay <anyan.chen@technicolor.com>
2021-01-11 09:38:14 +0800

    gdb core dump virutal ptr

---
+++ lang/dump.cpp
@@ -0,0 +1,50 @@
+// 作者：一颗大白菜
+// 链接：https : //zhuanlan.zhihu.com/p/46605905
+// 来源：知乎
+// 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+
+#include "stdio.h"
+#include <iostream>
+#include "stdlib.h"
+              using namespace std;
+class base
+{
+public:
+    base();
+    virtual void test();
+
+private:
+    char *basePStr;
+};
+class dumpTest : public base
+{
+public:
+    void test();
+
+private:
+    char *childPStr;
+};
+base::base()
+{
+    basePStr = "test_info";
+}
+void base::test()
+{
+    cout << basePStr << endl;
+}
+void dumpTest::test()
+{
+    cout << "dumpTest" << endl;
+    delete childPStr;
+}
+void dumpCrash()
+{
+    char *pStr = "test_content";
+    free(pStr);
+}
+int main()
+{
+    dumpTest dump;
+    dump.test();
+    return 0;
+}
