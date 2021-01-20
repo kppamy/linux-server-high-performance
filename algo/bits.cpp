c866de3690c4b63a5a350d64f99d2bf2f7503ae7
chenay <anyan.chen@technicolor.com>
2021-01-20 15:49:20 +0800

    // 191. Number of 1 Bits
    1.n&(n-1) can count bits
    2.bitset<32>(num).count()
    3.__builtin_popcount(n)

--- algo/bits.cpp
+++ algo/bits.cpp
@@ -1,5 +1,60 @@
 #include <iostream>
+#include <vector>
+#include <bitset>
 using namespace std;
+
+// 191. Number of 1 Bits
+//  0 ms, faster than 100.00%
+// 6 MB, less than 93.28%
+int hammingWeightP(uint32_t n)
+{
+    return __builtin_popcount(n);
+}
+
+// 191. Number of 1 Bits
+//  0 ms, faster than 100.00%
+// 6 MB, less than 93.28%
+int hammingWeightB(uint32_t n)
+{
+    bitset<32> bs(n);
+    return bs.count();
+}
+
+// 191. Number of 1 Bits
+// 0 ms, faster than 100.00%
+// 5.9 MB, less than 93.28%
+int hammingWeight(uint32_t n)
+{
+    int count = 0;
+    while (n)
+    {
+        n &= (n - 1);
+        count++;
+    }
+    return count;
+    // int weigh = 0;
+    // for (int i = 1; i <= 32 && n > 0; i++)
+    // {
+    //     int half = n >> 1;
+    //     if (n - (half << 1) == 1)
+    //         weigh++;
+    //     n = half;
+    // }
+    // return weigh;
+}
+
+void testhammingWeight()
+{
+    vector<uint32_t> cs = {
+        11,
+        128,
+        4294967293};
+
+    for (auto &&num : cs)
+    {
+        cout << num << "  hammingWeight is " << hammingWeight(num) << endl;
+    }
+}
 
 // 461. Hamming Distance
 //  0 ms, faster than 100.00%
@@ -36,6 +91,7 @@ void testhammingDistance()
 int main(int argc, char const *argv[])
 {
 
-    testhammingDistance();
+    // testhammingDistance();
+    testhammingWeight();
     return 0;
 }
