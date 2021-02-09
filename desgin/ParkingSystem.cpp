5ce582cf9722f9455b5e8d3d0fec26fcb0dd415f
chenay <anyan.chen@technicolor.com>
2021-02-09 17:34:59 +0800

    // 1656. Design an Ordered Stream

--- desgin/ParkingSystem.cpp
+++ desgin/ParkingSystem.cpp
@@ -1,9 +1,47 @@
 #include "../common.h"
 #include <array>
 using namespace std;
+
+// 1656. Design an Ordered Stream
+// 116 ms, faster than 75.03%
+// 83.7 MB, less than 72.86%
+class OrderedStream
+{
+public:
+    OrderedStream(int n) : size(n)
+    {
+        // ordered.resize(n + 1, "A"); change from "AAAAA" to "A" will be slower, aligned?
+        ordered.resize(n + 1, "AAAAA");
+    }
+
+    vector<string> insert(int id, string value)
+    {
+        ordered[id] = value;
+        vector<string> out;
+        while (ptr < size) //change size -1 to size, improve from 72.86% to 85.78%
+        {
+            string val = ordered[ptr + 1];
+            // if (val != "AAAAA")
+            if (val[0] != 'A') // compare one byte instead of 5 bytes, improved from 85.78% faster than 90.12%
+            {
+                ptr++;
+                out.push_back(val);
+            }
+            else
+                break;
+        }
+        return move(out);
+    }
+
+private:
+    vector<string> ordered;
+    int size{0};
+    int ptr = 0;
+};
+
 // 1603. Design Parking System
 // 48 ms, faster than 96.98%
-// 33.1 MB, less than 65.81% 
+// 33.1 MB, less than 65.81%
 class ParkingSystem
 {
 public:
@@ -27,5 +65,5 @@ public:
 
 private:
     array<int, 4> slots;
-    // array<int, 3> slots; // will be slower as substraction included 
+    // array<int, 3> slots; // will be slower as substraction included
 };
