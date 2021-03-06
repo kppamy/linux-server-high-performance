260afccc3637ddb5b2de7e93be1b20490fd12aa3
chenay <anyan.chen@technicolor.com>
2021-03-06 10:16:45 +0800

    // 63. Unique Paths II

--- algo/DynamicProgramming.cpp
+++ algo/DynamicProgramming.cpp
@@ -3,6 +3,39 @@
 #include <math.h>
 #include "../Timer.h"
 
+// 63. Unique Paths II
+// 0 ms, faster than 100.00%
+// 7.9 MB, less than 33.19%
+int uniquePathsWithObstacles(vector<vector<int>> &obstacleGrid)
+{
+    int m = obstacleGrid.size(), n = obstacleGrid[0].size();
+    vector<vector<int>> dp(m, vector<int>(n, 0));
+    for (int i = 0; i < n; i++)
+    {
+        if (obstacleGrid[0][i] == 0)
+            dp[0][i] = 1;
+        else
+            break;
+    }
+    for (int i = 0; i < m; i++)
+    {
+        if (obstacleGrid[i][0] == 0)
+            dp[i][0] = 1;
+        else
+            break;
+    }
+
+    for (int i = 1; i < m; i++)
+    {
+        for (int j = 1; j < n; j++)
+        {
+            if (obstacleGrid[i][j] == 0)
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+        }
+    }
+    return dp[m - 1][n - 1];
+}
+
 // 62. Unique Paths
 //  0 ms, faster than 100.00%
 //  6.4 MB, less than 37.91%
