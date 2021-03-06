a50d1c885e9dfe881b8eec126f7543b13d4d0d31
chenay <anyan.chen@technicolor.com>
2021-03-06 09:56:54 +0800

    // 62. Unique Paths

--- algo/DynamicProgramming.cpp
+++ algo/DynamicProgramming.cpp
@@ -3,6 +3,31 @@
 #include <math.h>
 #include "../Timer.h"
 
+// 62. Unique Paths
+//  0 ms, faster than 100.00%
+//  6.4 MB, less than 37.91%
+int uniquePaths(int m, int n)
+{
+    vector<vector<int>> dp(m, vector<int>(n, 0));
+    for (int i = 0; i < n; i++)
+    {
+        dp[0][i] = 1;
+    }
+    for (int i = 0; i < m; i++)
+    {
+        dp[i][0] = 1;
+    }
+
+    for (int i = 1; i < m; i++)
+    {
+        for (int j = 1; j < n; j++)
+        {
+            dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+        }
+    }
+    return dp[m - 1][n - 1];
+}
+
 // 64. Minimum Path Sum
 // 12 ms, faster than 66.01%
 // 10.2 MB, less than 35.67%
